06987cf47662f44db003e45bc694a201a07641b8
Devloper-solaiman <devlopersolaiman@gmail.com>
2023-05-09 12:40:01 +0600

    404 page create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
 import Blogs from './Pages/Blogs/Blogs';
 import Review from './Pages/Review/Review';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import About from './Pages/About/About';
+import PageNotFound from './Pages/PageNotFound/PageNotFound';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
         <Route path='/review' element={<Review></Review>}></Route>
         <Route path='/dashbord' element={<Dashboard></Dashboard>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path="*" element={<PageNotFound></PageNotFound>} />
+
       </Routes>
     </>
   );
